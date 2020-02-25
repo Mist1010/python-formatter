7e64c546db914289c2c32fac7f98b7499582348a
Ethan <52849211+Mist1010@users.noreply.github.com>
2020-02-25 14:57:07 -0600

    Comments
    Added comments and removed unnecessary code

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+# Imports the module to print a random choice from strings, can be removed if 'strings = random.choice(strings)' is removed
 import random
 
-def Convert(string): 
-    li = list(string.split(" ")) 
-    return li 
-
+# Base list, can add items before hand or keep it empty to only output the document
 strings = []
 
+# Appends every line of the document and strips it of unnecessary formats
 file = open('text.txt', 'r')
 for line in file:
   stng = line.strip()
   strings.append(stng)
 
+# Remove this line of code to just print the list and not one random
 strings = random.choice(strings)
+
+# Prints the final result of list
 print(strings)
